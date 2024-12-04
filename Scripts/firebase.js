468437c6a64db4958b2c18346d1dcceeeef99c6d
Rabia-Bhutto <rabiabhutto05@gmail.com>
2024-12-04 06:07:53 +0500

    First Commit

---
+++ Scripts/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+// import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4VWz9LPUFoNS8roYl7hRlUXADEJH3roA",
+  authDomain: "smit-mini-hackathon-4-12-24.firebaseapp.com",
+  projectId: "smit-mini-hackathon-4-12-24",
+  storageBucket: "smit-mini-hackathon-4-12-24.firebasestorage.app",
+  messagingSenderId: "164722399005",
+  appId: "1:164722399005:web:0beef19a1587d26ad473f4",
+  measurementId: "G-0T1CQLH67C"
+};
+
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export{
+    
+}
